012f29ef21cd7c7a072455d345797e15cc349af3
Makarov Andrey <xabor@yandex.ru>
2021-09-23 15:56:49 +0300

    Первый коммит

---
+++ yatube/posts/models.py
@@ -0,0 +1,40 @@
+# Create your models here.
+
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model() 
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    # Тип: TextField
+    text = models.TextField() 
+    
+    # Тип поля: DateTimeField, для хранения даты и времени;
+    # параметр auto_now_add определяет, что в поле будет автоматически
+    # подставлено время и дата создания новой записи
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    # Тип: ForeignKey, ссылка на модель User
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name = 'posts'
+    ) 
+
+    group = models.ForeignKey(
+        Group,
+        related_name='posts',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
